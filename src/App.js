d857a236de6ff1a099136fe17f82453123032fd9
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2023-01-13 20:10:40 -0500

    basic boiler react template set up with dependencies

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Container  from "react-bootstrap/Container";
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Container>
+
+      </Container>
+
+    </div>
+  )
+}
+
+export default App;
